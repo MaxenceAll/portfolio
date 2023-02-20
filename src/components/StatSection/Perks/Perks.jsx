fa2b3da5f985db6c21c960ee589e92bc3e6d133a
MaxenceAll <maxence.allart@gmail.com>
2023-02-21 00:15:22 +0100

    nothing new

--- src/components/StatSection/Perks/Perks.jsx
+++ src/components/StatSection/Perks/Perks.jsx
@@ -4,6 +4,7 @@ import "./Perks.css";
 import curieux from "../../../assets/Perks/curieux.png";
 import teamwork from "../../../assets/Perks/teamwork.gif";
 import logique from "../../../assets/Perks/logique.png";
+import management from "../../../assets/Perks/management.png";
 
 
 function Perks() {
@@ -20,11 +21,7 @@ function Perks() {
     "curieux": "curieux description",
     "teamwork": "team work description",
     "logique": "logique description",
-    "endurance": "10h de code par jour ? No problemo .... avec 10 redbull par heure.",
-    "charisma": "Lunettes geeks CHECK. Barbe geek CHECK. Chemise à carreaux geek CHECK.",
-    "intelligence": "Suffisamment intélligent pour demander à CHATgpt et relativement comprendre la réponse.",
-    "agility": "L'agilité ? C'est débrouissailler du code déprécié, explorer les nouveaux frameworks, avec rien d'autre que son fidèle IDEs dopé d'extensions. Et tout comme un survivaliste, toujours avoir un plan de secours (un dépôt GitHub).",
-    "luck": "Pas besoin de chance quand on trafique le Math.random().",
+    "management": "management description"
   };
 
   return (
@@ -35,10 +32,7 @@ function Perks() {
             <li onClick={() => handleLiClick(curieux)} className={activeTab === curieux ? "perks-active-tab" : ""}>Curieux.</li> 
             <li onClick={() => handleLiClick(teamwork)} className={activeTab === teamwork ? "perks-active-tab" : ""}>Travail d'équipe.</li>
             <li onClick={() => handleLiClick(logique)} className={activeTab === logique ? "perks-active-tab" : ""}>Logique.</li>
-            {/* <li onClick={() => handleLiClick(charisma)} className={activeTab === charisma ? "perks-active-tab" : ""}>Charisma <span className="specialValue">10</span></li> */}
-            {/* <li onClick={() => handleLiClick(intelligence)} className={activeTab === intelligence ? "perks-active-tab" : ""}>Intelligence <span className="specialValue">10</span></li> */}
-            {/* <li onClick={() => handleLiClick(agility)} className={activeTab === agility ? "perks-active-tab" : ""}>Agility <span className="specialValue">10</span></li> */}
-            {/* <li onClick={() => handleLiClick(luck)} className={activeTab === luck ? "perks-active-tab" : ""}>Luck <span className="specialValue">10</span></li> */}
+            <li onClick={() => handleLiClick(management)} className={activeTab === management ? "perks-active-tab" : ""}>Management.</li>
           </ul>
         </div>
 
