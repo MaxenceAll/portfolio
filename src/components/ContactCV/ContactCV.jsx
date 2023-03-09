c42e0360f51f15f3e300ade37634c4538359080a
MaxenceAll <maxence.allart@gmail.com>
2023-03-09 12:05:17 +0100

    download CV fix

--- src/components/ContactCV/ContactCV.jsx
+++ src/components/ContactCV/ContactCV.jsx
@@ -1,35 +1,38 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 import { FaDownload } from "react-icons/fa";
 import { HiMail } from "react-icons/hi";
-import './ContactCV.css'
+import "./ContactCV.css";
+import CV from "../../assets/CV/CV-allart-Maxence-v4.pdf";
 
 function ContactCV() {
-
-  function handleClick() {
-    const fileName = 'CV-allart-Maxence-v4.pdf'; 
-    const fileUrl = '../../assets/CV/'; 
-    fetch(fileUrl)
-      .then(response => response.blob())
-      .then(blob => {
-        const url = window.URL.createObjectURL(blob);
-        const a = document.createElement('a');
-        a.href = url;
-        a.download = fileName;
-        a.click();
-        window.URL.revokeObjectURL(url);
-      });
-  }
-
   return (
     <>
-      <div className='ContactCV'>
-        <div className="" onClick={handleClick}>
-          <NavLink target="_blank" to="https://maxenceall.github.io/ProjectCV/" className="LienversCV">
+      <div className="ContactCV">
+        <div className="">
+          <NavLink
+            target="_blank"
+            to="https://maxenceall.github.io/ProjectCV/"
+            className="LienversCV"
+          >
             <p>ALLART Maxence : Concepteur Developpeur d'Application</p>
-            <p>Télécharger CV :<FaDownload /></p>
+
+            <p>
+              Télécharger CV :
+              <a href={CV} download>
+                <FaDownload />
+              </a>
+            </p>
           </NavLink>
         </div>
-        <p className="mail-me" onClick={() => window.location.href = "mailto:maxence.allart@gmail.com?subject=A l'attention de ALLART Maxence"}>Contact : maxence.allart@gmail.com  <HiMail /></p>
+        <p
+          className="mail-me"
+          onClick={() =>
+            (window.location.href =
+              "mailto:maxence.allart@gmail.com?subject=A l'attention de ALLART Maxence")
+          }
+        >
+          Contact : maxence.allart@gmail.com <HiMail />
+        </p>
       </div>
     </>
   );
